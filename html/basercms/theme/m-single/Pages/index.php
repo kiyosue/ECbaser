e685efbf3d340f1c15a25ea68510e18b2f917d6d
kiyosue <kiyo@itm.co.jp>
2017-01-10 20:21:08 +0900

    ec-cube , baserCMSをインストール状態とした

---
+++ html/basercms/theme/m-single/Pages/index.php
@@ -0,0 +1,6 @@
+<!-- BaserPageTagBegin -->
+<?php $this->BcBaser->setTitle('') ?>
+<?php $this->BcBaser->setDescription('') ?>
+<?php $this->BcBaser->setPageEditLink(1) ?>
+<!-- BaserPageTagEnd -->
+
